5d244aa5c48bc6ce67a1eed56ae5154c7cda5ecb
Michael <michael@greenacorn.com.au>
2016-07-07 12:05:43 +1000

    Render previews according to formatter settings and render non-PDF files

--- src/Plugin/Field/FieldFormatter/PDFPreviewFormatter.php
+++ src/Plugin/Field/FieldFormatter/PDFPreviewFormatter.php
@@ -8,9 +8,9 @@ namespace Drupal\pdfpreview\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Plugin\Field\FieldFormatter\FileFormatterBase;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
-
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'pdfpreview' formatter.
@@ -88,32 +88,100 @@ class PDFPreviewFormatter extends ImageFormatter {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
+    $files = $this->getEntitiesToView($items, $langcode);
+
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
+    }
+    elseif ($image_link_setting == 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting('image_style');
+
+    // Collect cache tags to be added for each item in the field.
+    $cache_tags = array();
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $cache_tags = $image_style->getCacheTags();
+    }
 
     $generator = \Drupal::service('pdfpreview.generator');
 
-    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $file) {
+    foreach ($files as $delta => $file) {
+      $cache_contexts = array();
+      if (isset($link_file)) {
+        $image_uri = $file->getFileUri();
+        // @todo Wrap in file_url_transform_relative(). This is currently
+        // impossible. As a work-around, we currently add the 'url.site' cache
+        // context to ensure different file URLs are generated for different
+        // sites in a multisite setup, including HTTP and HTTPS versions of the
+        // same site. Fix in https://www.drupal.org/node/2646744.
+        $url = Url::fromUri(file_create_url($image_uri));
+        $cache_contexts[] = 'url.site';
+      }
+      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
       $item = $file->_referringItem;
-      $preview_uri = $generator->getPDFPreview($file);
-      $preview = \Drupal::service('image.factory')->get($preview_uri);
-      $elements[$delta] = array(
-        '#theme' => 'image',
-        '#uri' => $preview_uri,
-      );
-//      $elements[$delta]['#theme'] = 'image_style';
-//      $elements[$delta]['#style_name'] = 'large';
-      if ($preview->isValid()) {
-        $elements[$delta]['#width'] = $preview->getWidth();
-        $elements[$delta]['#height'] = $preview->getHeight();
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
+      if (isset($item->description)) {
+        $item_attributes['alt'] = $item->description;
+        $item_attributes['title'] = $item->description;
+      }
+      $item_attributes['class'][] = 'pdfpreview-file';
+
+      // Separate the PDF previews from the other files.
+      $show_preview = FALSE;
+      if ($file->getMimeType() == 'application/pdf') {
+        $preview_uri = $generator->getPDFPreview($file);
+        $preview = \Drupal::service('image.factory')->get($preview_uri);
+        if ($preview->isValid()) {
+          $show_preview = TRUE;
+          $item->uri = $preview_uri;
+          $item->width = $preview->getWidth();
+          $item->height = $preview->getHeight();
+          $elements[$delta] = array(
+            '#theme' => 'image_formatter',
+            '#item' => $item,
+            '#item_attributes' => $item_attributes,
+            '#image_style' => $image_style_setting,
+            '#url' => $url,
+            '#cache' => array(
+              'tags' => $cache_tags,
+              'contexts' => $cache_contexts,
+            ),
+          );
+        }
+      }
+      if (!$show_preview) {
+        $elements[$delta] = array(
+          '#theme' => 'file_link',
+          '#file' => $file,
+          '#description' => $item->description,
+          '#cache' => array(
+            'tags' => $file->getCacheTags(),
+          ),
+        );
       }
 
-      // Pass field item attributes to the theme function.
-      if (isset($item->_attributes)) {
-        $elements[$delta] += array('#attributes' => array());
-        $elements[$delta]['#attributes'] += $item->_attributes;
-        // Unset field item attributes since they have been included in the
-        // formatter output and should not be rendered in the field template.
-        unset($item->_attributes);
-      }
+//      $elements[$delta]['#theme_wrappers'][] = 'pdfpreview_formatter';
+      $elements[$delta]['#settings'] = $this->getSettings();
+      $elements[$delta]['#fid'] = $file->id();
     }
 
     return $elements;
